f70d1586c6f471ad311f527c5264ce73f7a9b006
chilukavinayak <chilukavinayak.p@gmail.com>
2019-06-28 02:00:57 +0530

    function retrun array

--- src/SlidingWindow/Pattern1.java
+++ src/SlidingWindow/Pattern1.java
@@ -1,5 +1,7 @@
 package SlidingWindow;
 
+import java.util.Arrays;
+
 /*
 
  Given an array, find the average of all subarrays of size ‘K’ in it.
@@ -13,22 +15,25 @@ package SlidingWindow;
  */
 
 public class Pattern1 {
-	public static void bruteForce(int[] arry,int k) {
+	public static double[] bruteForce(int[] arry,int k) {
+	 
 		int n = arry.length;
+		double[] result = new double[n-k+1];
 		if(n < k)
-			return;
+			return result;
 
 		for(int i = 0; i < n-k+1; i++ ) {
 			int total=0;
 			for(int j=i;j<k+i;j++) {
 				total += arry[j];
 			}
-			System.out.println("Avarage: "+(double)total/k);
+			result[i] = (double)total/k;
 		}
+		return result;
 
 	}
 	public static void main(String[] args) {
-		bruteForce(new int[] { 1, 3, 2, 6, -1, 4, 1, 8, 2 }, 5);
+		System.out.println("Average "+ Arrays.toString(bruteForce(new int[] { 1, 3, 2, 6, -1, 4, 1, 8, 2 }, 5)));
 
 	}
 
