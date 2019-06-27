59bcdd729d0acf596d447fa40b3b95878b1a499e
chilukavinayak <chilukavinayak.p@gmail.com>
2019-06-28 01:53:17 +0530

    BruteFore find the average of k subarray

--- src/SlidingWindow/Pattern1.java
+++ src/SlidingWindow/Pattern1.java
@@ -1,11 +1,35 @@
 package SlidingWindow;
 
-public class Pattern1 {
+/*
 
+ Given an array, find the average of all subarrays of size ‘K’ in it.
+
+ Array: [1, 3, 2, 6, -1, 4, 1, 8, 2], K=5
+
+ (1+3+2+6−1)/5=>2.2
+ (3+2+6−1+4)/5=>2.8
+ (2+6−1+4+1)/5=>2.4
+ ...
+ */
+
+public class Pattern1 {
+	public static void bruteForce(int[] arry,int k) {
+		int n = arry.length;
+		if(n < k)
+			return;
+
+		for(int i = 0; i < n-k+1; i++ ) {
+			int total=0;
+			for(int j=i;j<k+i;j++) {
+				total += arry[j];
+			}
+			System.out.println("Avarage: "+(double)total/k);
+		}
+
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World");
-		System.out.println("Hello World");
+		bruteForce(new int[] { 1, 3, 2, 6, -1, 4, 1, 8, 2 }, 5);
+
 	}
 
 }
