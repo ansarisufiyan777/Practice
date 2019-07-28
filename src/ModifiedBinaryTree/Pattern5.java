23caf2fc59d673c40a8991d9b82454490656ca44
chilukavinayak <chilukavinayak.p@gmail.com>
2019-07-28 20:00:06 +0530

    Number Range
    Problem Statement
    Given an array of numbers sorted in ascending order, find the range of a
    given number ‘key’. The range of the ‘key’ will be the first and last
    position of the ‘key’ in the array.
    
    Write a function to return the range of the ‘key’. If the ‘key’ is not
    present return [-1, -1].
    
    Example 1:
    
    Input: [4, 6, 6, 6, 9], key = 6
    Output: [1, 3]
    Example 2:
    
    Input: [1, 3, 8, 10, 15], key = 10
    Output: [3, 3]
    Example 3:
    
    Input: [1, 3, 8, 10, 15], key = 12
    Output: [-1, -1]

--- src/ModifiedBinaryTree/Pattern5.java
+++ src/ModifiedBinaryTree/Pattern5.java
@@ -1,14 +1,21 @@
 package ModifiedBinaryTree;
 /*  
 
-Given an infinite sorted array (or an array with unknown size), find if a given number ‘key’ is present in the array. 
-Write a function to return the index of the ‘key’ if it is present in the array, otherwise return -1.
+Given an array of numbers sorted in ascending order, find the range of a given number ‘key’. The range of the ‘key’ will be the first and last position of the ‘key’ in the array.
 
-Since it is not possible to define an array with infinite (unknown) size, 
-you will be provided with an interface ArrayReader to read elements of the array. ArrayReader.get(index) will return the number at index;
- if the array’s size is smaller than the index, it will return Integer.MAX_VALUE.
+Write a function to return the range of the ‘key’. If the ‘key’ is not present return [-1, -1].
+Example 1:
 
+Input: [4, 6, 6, 6, 9], key = 6
+Output: [1, 3]
+Example 2:
 
+Input: [1, 3, 8, 10, 15], key = 10
+Output: [3, 3]
+Example 3:
+
+Input: [1, 3, 8, 10, 15], key = 12
+Output: [-1, -1]
  */
 public class Pattern5 {
 
